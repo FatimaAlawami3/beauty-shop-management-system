7bf5e25d61284f505bc08c8a6d0869e93cb7bae5
Fatima Alawami <fatimaalaawami@gmail.com>
2026-01-06 23:06:59 +0300

    Add files via upload

---
+++ database/beauty_manger.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: beauty
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `manger`
+--
+
+DROP TABLE IF EXISTS `manger`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manger` (
+  `mfname` varchar(55) DEFAULT NULL,
+  `mlname` varchar(55) DEFAULT NULL,
+  `SSN` int NOT NULL,
+  `SEX` varchar(55) DEFAULT NULL,
+  `M_PASSWORD` varchar(55) DEFAULT NULL,
+  `ADDRESS` varchar(55) DEFAULT NULL,
+  `SALARY` int DEFAULT NULL,
+  `CONTACTNUM` int DEFAULT NULL,
+  `BIRTHDATE` varchar(55) DEFAULT NULL,
+  PRIMARY KEY (`SSN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manger`
+--
+
+LOCK TABLES `manger` WRITE;
+/*!40000 ALTER TABLE `manger` DISABLE KEYS */;
+INSERT INTO `manger` VALUES ('asd','as',1,'fe','h,','adf',578,849,'12'),('saad','m',130,'male','1234','damam',20000,555,'12');
+/*!40000 ALTER TABLE `manger` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-12  1:00:10
